81edd9022a8158d52d9f9204dc1d46f00b2cbab7
allenair <allenair@gmail.com>
2019-09-23 15:12:25 +0800

    0923

--- test/demo1909.py
+++ test/demo1909.py
@@ -9,6 +9,16 @@ import numpy as np
 import re
 
 
+def And(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x)+b
+    if tmp<=0:
+        return 0
+    else:
+        return 1
+
 def findUrl():
     pattern = re.compile('.*<a href="//www.zgelevator.com/news.*')
     with open('d:/spider.txt', 'r', encoding='utf-8') as fin:
@@ -30,8 +40,10 @@ def sent2vec():
 
     vect_list = np.array(vect_list)
     vect = vect_list.sum(axis=0)
-    print(vect / np.sqrt((vect**2).sum()))
+    print(vect / np.sqrt((vect ** 2).sum()))
 
 
 if __name__ == '__main__':
-    findUrl()
+    for xs in [(x1, x2) for x1 in range(2) for x2 in range(2)]:
+        print(xs, "->", And(xs[0], xs[1]))
+    
